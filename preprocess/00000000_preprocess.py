f7dffaea4c3089589713289b743c34bd3b914a5f
Noel Karlsson Johansson <noel.johansson00@gmail.com>
2023-04-06 19:02:40 +0900

    Update 00000000_preprocess.py

--- preprocess/00000000_preprocess.py
+++ preprocess/00000000_preprocess.py
@@ -1,4 +1,17 @@
 import argparse
+import sys
+sys.path.append("DescEmb-master/preprocess")
+sys.path.append("DescEmb-master/trainers")
+sys.path.append("DescEmb-master/datasets")
+from create_dataset import create_MIMIC_dataset, create_eICU_dataset
+from dataframe_gen import preprocess
+from numpy_convert import convert2numpy
+from preprocess_utils import label_npy_file
+import os
+
+import torch
+import torch.multiprocessing as mp
+from trainers import Trainer, Word2VecTrainer
 
 def get_parser():
     """
@@ -37,11 +50,127 @@ def main(args):
     root_dir = args.root
     dest_dir = args.dest
 
+
+    
+    mimic_csv_files = {'lab':'LABEVENTS', 
+                        'med':'PRESCRIPTIONS',
+                        'inf': 'INPUTEVENTS'}
+
+    eicu_csv_files = {'lab':'lab', 
+                    'med':'medication',
+                    'inf':'infusionDrug'}
+
+    # definition file name        
+    mimic_def_file = {'LABEVENTS':'D_LABITEMS', 
+                    'INPUTEVENTS_CV':'D_ITEMS', 
+                    'INPUTEVENTS_MV':'D_ITEMS'}
+
+    # columns_map
+    mimic_columns_map = {'LABEVENTS':
+                            {'HADM_ID':'ID',
+                            'CHARTTIME':'code_time',
+                            'ITEMID':'code_name',
+                            'VALUENUM':'value',
+                            'VALUEUOM':'uom',
+                            'FLAG':'issue'
+                            },
+                        'PRESCRIPTIONS':
+                            {'HADM_ID':'ID',
+                            'STARTDATE':'code_time',
+                            'DRUG':'code_name', 
+                            'ROUTE':'route', 
+                            'PROD_STRENGTH':'prod',
+                            'DOSE_VAL_RX':'value',
+                            'DOSE_UNIT_RX':'uom',
+                            },                                      
+                        'INPUTEVENTS': 
+                            {'HADM_ID':'ID',
+                            'CHARTTIME':'code_time', 
+                            'ITEMID':'code_name',
+                            'RATE':'value', 
+                            'RATEUOM':'uom',
+                            'STOPPED':'issue'
+                            }
+    }
+
+    eicu_columns_map =  {'lab':
+                            {'patientunitstayid':'ID', 
+                            'labresultoffset':'code_offset',
+                            'labname':'code_name',
+                            'labresult':'value',
+                            'labmeasurenamesystem':'uom'
+                            },
+                        'medication':
+                            {'patientunitstayid':'ID',
+                            'drugstartoffset':'code_offset',
+                            'drugname':'code_name', 
+                            'routeadmin':'route',
+                            'ordercancelled':'issue'
+                            },      
+                        'infusionDrug':
+                            {'patientunitstayid':'ID',
+                            'infusionoffset':'code_offset',
+                            'drugname':'code_name',
+                            'infusionrate':'value'
+                            }
+    }
+   
+    issue_map = {'LABEVENTS': 
+                    ['abnormal'],                            
+                'INPUTEVENTS':
+                    ['Restart',
+                    'NotStopd', 
+                    'Rewritten', 
+                    'Changed', 
+                    'Paused', 
+                    'Flushed', 
+                    'Stopped'
+                    ] ,
+                'medication': 
+                    ['Yes'],          
+                }
+    
+
+    csv_files_dict = {'mimic':mimic_csv_files, 
+                        'eicu':eicu_csv_files
+    }
+    columns_map_dict = {'mimic':mimic_columns_map, 
+                           'eicu':eicu_columns_map
+    }
+    item_list = ['lab','med', 'inf']
+    wd = os.getcwd()
+    print('working directory .. : ', wd)
+
+    #create_MIMIC_dataset(os.path.join(args.data_input_path, 'mimic'))
+    #create_eICU_dataset(os.path.join(args.data_input_path, 'eicu'))
+
+    # preprocess(args.root, 
+    #                 item_list,
+    #                csv_files_dict, 
+    #                columns_map_dict, 
+    #                issue_map, 
+    #                mimic_def_file,
+    #                150,
+    #                'MICU')
+    
+    args = argparse.Namespace()
+    args.batch_size = 128
+    args.value_embed_type = 'nonconcat'
+    args.data = "train/"
+    args.save_dir = "checkpoint"
+    args.save_prefix = "checkpoint"
+    args.epoch = 10
+
+    trainer = Word2VecTrainer(args)
+    trainer.train()
+
+
+    
 if __name__ == "__main__":
     #parser = get_parser()
     #args = parser.parse_args()
-    args = argparse.ArgumentParser()
-    args.root = "./train"
-    args.dest = "./output"
+    args = argparse.Namespace()
+    args.root = "train/"
+    args.dest = "output/"
 
     main(args)
